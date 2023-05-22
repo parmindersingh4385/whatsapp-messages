7d3c63ada6829b8c7e3f93d7060922e1f5e78a7c
Parminder singh <singh1.parminder@gmail.com>
2023-05-22 15:02:56 +0530

    no message

--- index.js
+++ index.js
@@ -13,81 +13,81 @@ const port = process.env.PORT || 5000;
 app.get('/', function (req, res) {
 	res.send({
 		success: true,
-		message: 'WhatsApp message sent successfully.............666'
+		message: 'WhatsApp message sent successfully.............777'
 	});
 });
 
 // Load the session data
 
-// mongoose
-// 	.connect(
-// 		'mongodb+srv://parminder:9988641591%40ptk@cluster0-ix992.mongodb.net/db_products?retryWrites=true&w=majority',
-// 		{
-// 			useNewUrlParser: true,
-// 			useUnifiedTopology: true
-// 		}
-// 	)
-// 	.then(() => {
-// 		console.log('CONNECT.........................');
-// 		const store = new MongoStore({ mongoose: mongoose });
-// 		const client = new Client({
-// 			authStrategy: new RemoteAuth({
-// 				store: store,
-// 				backupSyncIntervalMs: 300000
-// 			})
-// 		});
+mongoose
+	.connect(
+		'mongodb+srv://parminder:9988641591%40ptk@cluster0-ix992.mongodb.net/db_products?retryWrites=true&w=majority',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		}
+	)
+	.then(() => {
+		console.log('CONNECT.........................');
+		const store = new MongoStore({ mongoose: mongoose });
+		const client = new Client({
+			authStrategy: new RemoteAuth({
+				store: store,
+				backupSyncIntervalMs: 300000
+			})
+		});
 
-// 		client.on('qr', (qr) => {
-// 			qrcode.generate(qr, { small: true });
-// 		});
+		client.on('qr', (qr) => {
+			qrcode.generate(qr, { small: true });
+		});
 
-// 		client.on('ready', () => {
-// 			console.log('Client is ready!');
-// 			schedule.scheduleJob('*/1 * * * *', function () {
-// 				console.log('schedule.........................');
-// 				client.getChats().then(function (chats) {
-// 					const chatGroup = chats.find(
-// 						(chat) => chat.name == 'GirlsFab'
-// 					);
-// 					console.log(chatGroup.id._serialized);
-// 					client.sendMessage(
-// 						chatGroup.id._serialized,
-// 						'Number is ' + Math.floor(Math.random() * 10)
-// 					);
-// 					//sendImage(chatGroup);
-// 				});
-// 			});
-// 			// cron.schedule('*/59 * * * * *', function () {
-// 			// 	client.getChats().then(function (chats) {
-// 			// 		const chatGroup = chats.find(
-// 			// 			(chat) => chat.name == 'GirlsFab'
-// 			// 		);
-// 			// 		//console.log(chatGroup);
-// 			// 		// client.sendMessage(
-// 			// 		// 	chatGroup.id._serialized,
-// 			// 		// 	'Number is ' + Math.floor(Math.random() * 10)
-// 			// 		// );
-// 			// 		sendImage(chatGroup);
-// 			// 	});
-// 			// });
-// 		});
+		client.on('ready', () => {
+			console.log('Client is ready!');
+			schedule.scheduleJob('*/1 * * * *', function () {
+				console.log('schedule.........................');
+				client.getChats().then(function (chats) {
+					const chatGroup = chats.find(
+						(chat) => chat.name == 'GirlsFab'
+					);
+					console.log(chatGroup.id._serialized);
+					client.sendMessage(
+						chatGroup.id._serialized,
+						'Number is ' + Math.floor(Math.random() * 10)
+					);
+					//sendImage(chatGroup);
+				});
+			});
+			// cron.schedule('*/59 * * * * *', function () {
+			// 	client.getChats().then(function (chats) {
+			// 		const chatGroup = chats.find(
+			// 			(chat) => chat.name == 'GirlsFab'
+			// 		);
+			// 		//console.log(chatGroup);
+			// 		// client.sendMessage(
+			// 		// 	chatGroup.id._serialized,
+			// 		// 	'Number is ' + Math.floor(Math.random() * 10)
+			// 		// );
+			// 		sendImage(chatGroup);
+			// 	});
+			// });
+		});
 
-// 		async function sendImage(chatGroup) {
-// 			console.log('sendImage....................');
-// 			const media = await MessageMedia.fromUrl(
-// 				'https://m.media-amazon.com/images/I/91p5L+GitZL._SX569_.jpg'
-// 			);
-// 			client.sendMessage(chatGroup.id._serialized, media, {
-// 				caption: 'https://amzn.to/3BGm8Yw'
-// 			});
-// 		}
+		async function sendImage(chatGroup) {
+			console.log('sendImage....................');
+			const media = await MessageMedia.fromUrl(
+				'https://m.media-amazon.com/images/I/91p5L+GitZL._SX569_.jpg'
+			);
+			client.sendMessage(chatGroup.id._serialized, media, {
+				caption: 'https://amzn.to/3BGm8Yw'
+			});
+		}
 
-// 		client.on('remote_session_saved', () => {
-// 			console.log('remote_session_saved....................1234567890');
-// 		});
+		client.on('remote_session_saved', () => {
+			console.log('remote_session_saved....................1234567890');
+		});
 
-// 		client.initialize();
-// 	});
+		client.initialize();
+	});
 
 app.listen(port, '0.0.0.0', function () {
 	console.log('App running on port ' + port);
