1b6352aebe8bcf2f37e9c4902a9cb498d2ae7e03
Parminder singh <singh1.parminder@gmail.com>
2023-05-22 14:01:15 +0530

    no message

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', function (req, res) {
+	res.send({
+		success: true,
+		message: 'WhatsApp message sent successfully'
+	});
+});
+
+app.listen(port, '0.0.0.0', function () {
+	console.log('App running on port ' + port);
+});
