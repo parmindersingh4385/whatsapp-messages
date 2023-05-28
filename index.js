5792ea6cb49f8c21ada2b73bbfc595e30c7c208f
Parminder singh <singh1.parminder@gmail.com>
2023-05-28 09:37:02 +0530

    no message

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.get('/', function (req, res) {
 	res.send({
 		success: true,
-		message: 'WhatsApp message sent successfully.............5:55 pm'
+		message: 'WhatsApp message sent successfully.............10 AM'
 	});
 });
 
